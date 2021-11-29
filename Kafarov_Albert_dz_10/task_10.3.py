41cb361cd0d7ebec9a95f5bcea12252935d9bf9b
Kafarov <kilopechenya@gmail.com>
2021-11-29 21:58:41 +0400

    Доработка дз 10ю.3

--- Kafarov_Albert_dz_10/task_10.3.py
+++ Kafarov_Albert_dz_10/task_10.3.py
@@ -2,8 +2,19 @@
 
 class Cell:
 
+    def __new__(cls, integer):
+        try:
+            if integer <= 0:
+                print('Клетка не жизнеспособна')
+                return None
+            else:
+                return super().__new__(cls)
+        except AttributeError:
+            print('Клетка не жизнеспособна')
+
     def __init__(self, integer):
         self.integer = int(integer)
+        print('Создана клетка', self.integer)
 
     def __add__(self, other):
         return self.integer + other.integer
@@ -12,7 +23,7 @@ class Cell:
         if self.integer > other.integer:
             return self.integer - other.integer
         else:
-            return 'Исходная клетка меньше, невозможно вычесть'
+            return 0
 
     def __mul__(self, other):
         return self.integer * other.integer
@@ -35,7 +46,11 @@ print(cell_1.make_order(5))
 print(cell_2.make_order(3))
 print(cell_3.make_order(5))
 
-print(cell_1 + cell_3)
-print(cell_3 / cell_1)
-print(cell_2 - cell_2)
-print(cell_1 * cell_3)
+cell_4 = Cell(cell_1 + cell_3)
+cell_5 = Cell(cell_3 / cell_1)
+cell_6 = Cell(cell_2 - cell_2)
+cell_7 = Cell(cell_1 * cell_3)
+print(cell_4.make_order(5))
+print(cell_5.make_order(5))
+# print(cell_6.make_order(5))
+print(cell_7.make_order(5))
